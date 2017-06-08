af1fdbc8570f06199940299860ef7ecd03131a0e
AnkitPandey14ap <ankitpandey14ap@gmail.com>
2017-06-08 22:58:21 +0530

    interval issue audio is working

--- app/src/main/java/com/example/ankit/alarm_walktooff/MainActivity.java
+++ app/src/main/java/com/example/ankit/alarm_walktooff/MainActivity.java
@@ -1,27 +1,41 @@
 package com.example.ankit.alarm_walktooff;
 
+import android.Manifest;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.os.Environment;
 import android.os.SystemClock;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ButtonBarLayout;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 20;
     private Button startButton;
     private Button stopButton;
     private Button startAtButton;
+    private EditText hourEditText;
+    private EditText minutesEditText;
     private PendingIntent pendingIntent;
+    File folder;
+    String song;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,11 +45,84 @@ public class MainActivity extends AppCompatActivity {
         stopButton = (Button) findViewById(R.id.stopButton);
         startAtButton = (Button) findViewById(R.id.startAtButton);
 
+
+
+
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+                Log.i("Ankit", "inner if");
+
+            } else {
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+                Log.i("Ankit", "requsted READ PHONE_STATE");
+
+
+            }
+        }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECORD_AUDIO)) {
+
+                Log.i("Ankit", "inner if");
+
+            } else {
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+                Log.i("Ankit", "requsted READ PHONE_STATE");
+
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        folder = new File(Environment.getExternalStorageDirectory() + "/"+"Empire.mp3");
+        song=folder.toString();
+
         //It will perform  a broadcast
 /*
         Intent alarmIntent=new Intent(this, MainActivity.class);
         pendingIntent = PendingIntent.getActivity(this, 0, alarmIntent, 0);
 */
+        play(song);
 
         Intent intent = new Intent(this, MainActivity.class);
         intent.setAction(Intent.ACTION_MAIN);
@@ -107,6 +194,8 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Ankit", "set");
         Toast.makeText(this, "Alarm set", Toast.LENGTH_SHORT).show();
 
+        play(song);
+
     }
 
     private void stop() {
@@ -116,18 +205,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startAt() {
+        hourEditText = (EditText) findViewById(R.id.hourEditText);
+        minutesEditText = (EditText) findViewById(R.id.minutesEditText);
+
+        int hh = Integer.parseInt(hourEditText.getText().toString());
+        int mm = Integer.parseInt(minutesEditText.getText().toString());
         AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         int interval = 1000 * 60 * 20;
 
         /* Set the alarm to start at 10:30 AM */
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.set(Calendar.HOUR_OF_DAY, 4);
-        calendar.set(Calendar.MINUTE, 32);
+        calendar.set(Calendar.HOUR_OF_DAY, hh);
+        calendar.set(Calendar.MINUTE, mm);
 
         /* Repeating on every 20 minutes interval */
         manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
                 1000 * 60 * 20, pendingIntent);
+
+    }
+    void play(String mFileName){
+        MediaPlayer mediaPlayer=new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource(mFileName);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
